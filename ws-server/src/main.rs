8fb030d082bb9b3e230db959ebb59132299c212c
kitune-chan-250r <nekoo.hisui@gmail.com>
2025-08-25 13:01:36 +0000

    最小構成のaxumアプリを実装

--- ws-server/src/main.rs
+++ ws-server/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a route
+    let app = Router::new().route("/", get(health_check_handler));
+
+    // run it
+    let listener = TcpListener::bind("0.0.0.0:8000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+/**
+ * health check用エンドポイント
+ */
+async fn health_check_handler() -> &'static str {
+    "Hello, World!"
 }
